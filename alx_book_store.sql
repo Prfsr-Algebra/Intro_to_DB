7e93ae190ff726d08b278bd5c195c1af52c54a80
Prfsr-Algebra <himsluve@gmail.com>
2025-02-22 20:58:38 -0800

    task 0 edited

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE Books(
 book_id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(130) NOT NULL,
-author_id INT FOREIGN KEY,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 price DOUBLE NOT NULL,
 publication_date DATE NOT NULL
 )
@@ -18,12 +18,12 @@ address TEXT NOT NULL
 )
 CREATE TABLE Orders(
 order_id INT AUTO_INCREMENT PRIMARY KEY,
-customer_id INT FOREIGN KEY,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 order_date DATE NOT NULL
 )
 CREATE TABLE Order_Details(
 order_details_id INT AUTO_INCREMENT PRIMARY KEY,
-order_id INT FOREIGN KEY,
-book_id INT FOREIGN KEY,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 quantity DOUBLE NOT NUL
 )
